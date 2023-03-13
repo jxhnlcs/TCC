670ac4da5dccdd823009cba3c79bd8fd39521b0f
John Lucas <joohnzay@gmail.com>
2023-03-13 10:17:39 -0300

    Add modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,20 @@ function colorLink(){
     }
 }
 linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+
+/* Modal */
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
